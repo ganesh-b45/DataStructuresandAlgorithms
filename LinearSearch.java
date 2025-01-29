132a14eebe1403b2e9b70b9c208ba2b461d20b47
Ganesh Bellale <ganeshbellale45@gmail.com>
2025-01-29 12:22:13 +0530

    LinearSearch

---
+++ LinearSearch.java
@@ -0,0 +1,23 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // Return the index of the target element
+            }
+        }
+        return -1; // Return -1 if the element is not found
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int target = 30;
+
+        int index = linearSearch(numbers, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
