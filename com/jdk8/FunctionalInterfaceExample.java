fcbc2fd4f79debc4eab9fea2890836a245fd8b64
Ganesh Bellale <ganeshbellale45@gmail.com>
2025-01-30 12:39:06 +0530

    modified logic

---
+++ com/jdk8/FunctionalInterfaceExample.java
@@ -0,0 +1,14 @@
+package com.jdk8;
+
+@FunctionalInterface
+interface StringProcessor {
+    int getLength(String str);
+}
+
+
+public class FunctionalInterfaceExample {
+    public static void main(String[] args) {
+        StringProcessor processor = str->{return str.length();};
+        System.out.println("Length: " + processor.getLength("Hello Java 8!"));
+    }
+}
