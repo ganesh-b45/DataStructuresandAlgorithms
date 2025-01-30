545951415593cf2a5f32059eb0e4897de506f11b
Ganesh Bellale <ganeshbellale45@gmail.com>
2025-01-30 13:06:25 +0530

    ConvertStringUpperCaseByStreamAPI

---
+++ com/jdk8/ConvertStringUpperCaseByStreamAPI.java
@@ -0,0 +1,16 @@
+package com.jdk8;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ConvertStringUpperCaseByStreamAPI {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("java", "spring", "hibernate");
+
+//        using lamda expression
+//        names.stream().map(str->str.toUpperCase()).forEach(System.out::println);
+        
+//        using method reference
+        names.stream().map(String::toUpperCase).forEach(System.out::println);
+    }
+}
