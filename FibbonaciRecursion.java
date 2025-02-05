a2a5d4365918dca889eebad2c4fe6352ae6d9754
Ganesh Bellale <ganeshbellale45@gmail.com>
2025-02-05 22:33:38 +0530

    FibbonaciRecursion

--- FibbonaciRecursion.java
+++ FibbonaciRecursion.java
@@ -1,7 +1,22 @@
 import static java.lang.System.*;
 
 public class FibbonaciRecursion {
+    int f1 = 0, f2 = 1, fib = 0;
+    public void fib(int n) {
+        if (n >= 1) {
+            fib = f1 + f2;
+            out.print(fib + "\t");
+            f1 = f2;
+            f2 = fib;
+            fib(n - 1);
+        }
+    }
     public static void main(String[] args) {
+
+        FibbonaciRecursion obj = new FibbonaciRecursion();
+        out.print(obj.f1 + "\t" + obj.f2 + "\t");
+        obj.fib(10);
+/*
         int f1 = 0, f2 = 1, fib = 0;
         out.print(f1 + "\t" + f2 + "\t");
         for (int i = 1; i <= 10; i++) {
@@ -10,5 +25,6 @@ public class FibbonaciRecursion {
             f1 = f2;
             f2 = fib;
         }
+*/
     }
 }
