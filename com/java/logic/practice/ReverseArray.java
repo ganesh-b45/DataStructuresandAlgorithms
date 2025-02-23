e3b6e83e4583d681406617e1292c494b7f21a7ab
Ganesh Bellale <ganeshbellale45@gmail.com>
2025-02-23 08:34:26 +0530

    Modified some logic

--- com/java/logic/practice/ReverseArray.java
+++ com/java/logic/practice/ReverseArray.java
@@ -3,16 +3,19 @@ package com.java.logic.practice;
 public class ReverseArray {
     public static void main(String[] args) {
         int a[]={5,4,3,2,1};
+        int b[]={10,9,8,7,6};
+        int[] newArray= new int[a.length+b.length];
 
-        int[] newArray= new int[a.length];
-        int index=0;
 
-        for (int i = a.length-1; i >=0 ; i--) {
-            newArray[index++]=a[i];
 
+        for (int i = 0; i < b.length; i++) {
+            newArray[i]=b[i];
         }
-
-         for(int i=0;i<newArray.length;i++)
-        System.out.println(newArray[i]);
+        for (int i = 0; i < a.length; i++) {
+            newArray[b.length+i]=a[i];
+        }
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.println(newArray[i]);
+        }
     }
 }
